e577db502c3a9aadcc2291d8fb2451fc34693114
Peter Olsen <olse0321@umn.edu>
2024-08-03 23:13:50 -0500

    Standardized inputs, added 'valid function' parse

--- src/utils/parsers.rs
+++ src/utils/parsers.rs
@@ -1,7 +1,18 @@
 /**
+ * The purpose of this module is to provide single line parsers to extract data or 
+ */
+
+
+/**
  * @purpose - Helper function to parse the arguments of a function signature
  */
-pub fn parse_params(signature: &str) -> Vec<String> {
+pub fn parse_params(line: &str) -> Vec<String> {
+    //extract function signature from the line
+    let line_split: Vec<&str> = line.split("function")
+        .into_iter()
+        .collect();
+    let signature = line_split[1];
+
     //find the parenthesis
     let first_parenthesis = signature
         .find('(')
@@ -16,19 +27,57 @@ pub fn parse_params(signature: &str) -> Vec<String> {
         .map(|s| s.trim().to_string())
         .collect();
 
-    println!("{:?}", params);
-
     params
 }
 
 /**
  * @purpose - Function to get the name from a signature string
  */
-pub fn parse_name(signature: &str) -> String {
+pub fn parse_name(line: &str) -> String {
+    //extract function signature from the line
+    let line_split: Vec<&str> = line.split("function")
+        .into_iter()
+        .collect();
+    let signature = line_split[1];
+
     let split_line: Vec<String> = signature
         .split("(")
         .map(|s| s.trim().to_string())
         .collect();
 
     split_line[0].clone()
+}
+
+/**
+ * Function to check if this is actually a function definition
+ * AKA make sure that it is not wrapped in quotes / comments
+ */
+pub fn parse_valid_function(line: &str) -> bool {
+    if !line.contains("function") {
+        false
+    }
+    else {
+        let function_split: Vec<&str> = line.split("function")
+            .into_iter()
+            .collect();
+
+        //function_split[0] will be empty if no text comes before 'function'
+        if function_split[0].is_empty() {
+            true
+        }
+        else {
+            //case where there are things before 'function', we need to check 'em
+            let part = function_split[0];
+
+            if part.contains("//") {
+                return false;
+            }
+
+            //check if there are an even number of quotation marks
+            if part.chars().filter(|&c| c == '\'' || c == '"').count() % 2 != 0 {
+                return false;
+            }
+            false
+        }
+    }
 }
