47d597e21b04129cbaa2f3a7563d8eea3002e273
Peter Olsen <olse0321@umn.edu>
2024-08-04 22:08:48 -0500

    Update CLI interfacing

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,38 @@ mod utils;
 
 use std::io::Result;
 use finders::{find_all_definitions, find_single};
-use std::env;
 use utils::parsers::CliParser;
 
 
 fn main() -> Result<()> {
-    let parser = utils::parsers::CliParser::new().parser;
-    let matches = parser.get_matches();
-    
-    let args: Vec<String> = env::args().collect();
+    let mut cli_parser = utils::parsers::CliParser::new();
+    let matches = cli_parser.parser.get_matches();
 
 
-    // let _result = find_all_definitions("test.js");
-    // let _result = find_all_definitions("nofunctions.js");
-    let _result = find_all_definitions("onefunction.js");
-
-
-    //two extra arguments, filename and function to find?
-    //or also maybe unsupported features so far
-    if args.len() == 3 {
-        //check if they want to find in a js file
-        match args[1].as_str() {
-            "findall" if args[2].ends_with(".js") => println!("Find all occurences of some function in all files!"),
-            _ if args[2].ends_with(".js") => find_single(&args[2], &args[1])?,
-            _ => println!("End of the pattern"),
-        }
+    //parse all arguments to the command line
+    //filename
+    if let Some(file) = matches.get_one::<String>("file") {
+        cli_parser.filename = Some(String::from(file));
+    } else {
+        cli_parser.filename = None;
     }
+
+    //function
+    if let Some(function) = matches.get_one::<String>("function") {
+        cli_parser.function = Some(String::from(function));
+    } else {
+        cli_parser.function = None;
+    }
+
+    //directory
+    if let Some(directory) = matches.get_one::<String>("directory") {
+        cli_parser.directory = Some(String::from(directory));
+    } else {
+        cli_parser.directory = None;
+    }
+    
+    //
+
 
     Ok(())
 }
