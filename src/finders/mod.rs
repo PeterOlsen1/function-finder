70305ff2511cfc35bd9842cc2283f8ead78b2a91
Peter Olsen <olse0321@umn.edu>
2024-08-08 22:08:14 -0500

    update modfile

--- src/finders/mod.rs
+++ src/finders/mod.rs
@@ -2,5 +2,5 @@ pub mod find_single;
 pub mod find_all_defs;
 pub mod find_all_directory;
 
-pub use find_single::find_single;
+pub use find_single::show_single;
 pub use find_all_defs::{find_all_definitions, all_definitions};
